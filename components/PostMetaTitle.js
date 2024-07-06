321dd3baaacfc3c45cf8ef1c0286f70876370b7d
Classic Values <classicvaluesllc@gmail.com>
2024-07-05 18:37:34 -0700

    COMMIT

--- components/PostMetaTitle.js
+++ components/PostMetaTitle.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function PostMetaTitle({ category, title, date, center }) {
   return (
     <>
-      <div className="flex items-center text-white/60 space-x-4">
-        <div className="uppercase">
+      <div className="flex items-center text-gray-500 space-x-4">
+        <div className="uppercase font-semibold">
           {category}
         </div>
         <span>&bull;</span>
@@ -12,8 +12,8 @@ export default function PostMetaTitle({ category, title, date, center }) {
           {date}
         </div>
       </div>
-      <h2 className={`text-2xl mt-4 ${center ? 'text-center' : ''}`}>
-        <Link href="/detail"><a>{title}</a></Link>
+      <h2 className={`text-2xl mt-4 font-bold ${center ? 'text-center' : ''}`}>
+        <Link href="/detail"><a className="text-primary hover:underline">{title}</a></Link>
       </h2>
     </>
   );
