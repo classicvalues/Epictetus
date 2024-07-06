e4f1e5bac626bafaa907f08e8e64742b1db3c3a4
Classic Values <classicvaluesllc@gmail.com>
2024-07-05 18:22:40 -0700

    COMMIT

--- pedago_api/services/course_service.py
+++ pedago_api/services/course_service.py
@@ -1,1 +1,17 @@
 
+courses = {}
+
+class CourseService:
+    @staticmethod
+    def get_all_courses():
+        return courses.values()
+
+    @staticmethod
+    def create_course(data):
+        course_id = len(courses) + 1
+        courses[course_id] = {
+            'course_id': course_id,
+            'title': data['title'],
+            'description': data['description'],
+            'instructor_id': data['instructor_id'],
+        }
