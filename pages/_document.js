65778032ae6285fe441daee5f6fa472138082400
Classic Values <classicvaluesllc@gmail.com>
2024-07-05 17:48:01 -0700

    COMMIT
    _document.js
    Set up custom document settings for SEO and performance.

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,27 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
           <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap" rel="stylesheet" />
         </Head>
-        <body>
+        <body className="bg-gray-50 text-gray-800 dark:bg-gray-900 dark:text-gray-200">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
-
+export default MyDocument;
