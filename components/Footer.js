d5a993e459196040a937398a71442bff8019651c
Classic Values <classicvaluesllc@gmail.com>
2024-07-05 18:36:23 -0700

    COMMIT

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="py-16 text-center text-white/60">
-      Copyright (c) 2021 - Design By Nauval
+    <footer className="py-16 text-center text-gray-700">
+      <p>
+        &copy; {new Date().getFullYear()} Epictetus - Free Education for Everyone. Design by Nauval
+      </p>
     </footer>
   );
 }
