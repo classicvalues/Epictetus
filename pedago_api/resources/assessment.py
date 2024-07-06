1f9ac9e705659078bbb17983fd0bcec090a7e1d5
Classic Values <classicvaluesllc@gmail.com>
2024-07-05 18:06:30 -0700

    COMMIT

--- pedago_api/resources/assessment.py
+++ pedago_api/resources/assessment.py
@@ -1,1 +1,15 @@
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required
+from services.assessment_service import AssessmentService
+
+class AssessmentResource(Resource):
+    @jwt_required()
+    def get(self):
+        return AssessmentService.get_all_assessments(), 200
+
+    @jwt_required()
+    def post(self):
+        data = request.get_json()
+        AssessmentService.create_assessment(data)
+        return {'message': 'Assessment created successfully'}, 201
 
