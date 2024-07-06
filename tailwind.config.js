925f074ea449ecea6a3f4904d5994d89a4d10fda
Classic Values <classicvaluesllc@gmail.com>
2024-07-05 18:38:22 -0700

    COMMIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,51 +20,26 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Patrick Hand', 'Arial', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-        mono: ['Fira Code', 'monospace'],
       },
       colors: {
-        primary: '#2563EB',
-        secondary: '#1E40AF',
-        accent: '#22D3EE',
-        background: '#E1E8ED',
-        surface: '#F5F8FA',
-      },
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
+        primary: '#3b82f6',
+        secondary: '#fb923c',
+        surface: '#ffffff',
+        background: '#f3f4f6',
+        text: '#1f2937',
       },
     },
   },
   variants: {
-    extend: {
-      backgroundColor: ['active'],
-      opacity: ['disabled'],
-      cursor: ['disabled'],
-    },
+    extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    plugin(({ addUtilities }) => {
-      const utilities = {
-        '.bg-search': {
-          backgroundImage: `url("data:image/svg+xml,%3Csvg width='14' height='14' viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg opacity='0.1'%3E%3Cpath d='M6.41667 11.0833C8.994 11.0833 11.0833 8.994 11.0833 6.41667C11.0833 3.83934 8.994 1.75 6.41667 1.75C3.83934 1.75 1.75 3.83934 1.75 6.41667C1.75 8.994 3.83934 11.0833 6.41667 11.0833Z' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/%3E%3Cpath d='M12.25 12.25L9.71251 9.71246' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/g%3E%3C/svg%3E%0A")`,
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: '20px',
-        },
-        '.bg-logo': {
-          backgroundImage: `url("/images/logo.svg")`,
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-        },
-      };
-
-      addUtilities(utilities);
-    }),
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+      });
+    })
   ],
 };
